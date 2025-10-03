8cdf580b31ae0a8ea264db7a3613db21d9b3d240
RuudErens <r.m.erens@student.tue.nl>
2025-10-03 10:59:52 +0900

    addition of nail and completeing HammeringTaskNew.cpp

--- src/HammeringTaskNew.cpp
+++ src/HammeringTaskNew.cpp
@@ -1,8 +1,12 @@
 #include "HammeringTaskNew.h"
+#include <mc_rbdyn/RobotLoader.h>
 
 HammeringTaskNew::HammeringTaskNew(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
 : mc_control::fsm::Controller(rm, dt, config)
 {
+
+  datastore().make<std::string>("ControlMode", "Torque");
+  datastore().make<std::string>("Coriolis", "Yes"); 
 
   mc_rtc::log::success("HammeringTaskNew init done ");
 }
